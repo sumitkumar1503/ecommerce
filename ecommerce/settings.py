8942b7876c11fd51e2226f7dd5b29d2d5453e920
sumit <wapka1503@gmail.com>
2020-09-08 09:34:36 +0530

    changed to sqlite3

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -81,12 +81,8 @@ WSGI_APPLICATION = 'ecommerce.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'ecom',
-        'USER': 'root',
-        'PASSWORD': 'root',
-        'HOST': 'localhost',
-        'PORT': '3306',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
